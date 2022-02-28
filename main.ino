415041b3aeb435b498e57723724fe9952423b18a
Dylan Carter <dylancarter@Dylans-MBP.attlocal.net>
2022-02-27 22:00:23 -0500

    planning stage 1

---
+++ main.ino
@@ -0,0 +1,18 @@
+//allow user input for delay times
+//color changes
+//velocity changes intensity
+
+
+
+int main(void){
+
+    //init interrupt which should trigger on 
+
+    //define ISR
+
+    //define array for each LED's intensity
+
+    while(1){
+        //place code to change int values here
+    }
+}
